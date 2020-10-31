0e0ea3b7a9f1ea56e972e58b7fcb77666caa81ee
vaibhav130901 <vaibhavsandeepnidhi@gmail.com>
2020-10-31 07:50:55 +0530

    Modified subject controller

--- controllers/subject.js
+++ controllers/subject.js
@@ -4,7 +4,21 @@ exports.getAllSubjects = async(req,res)=>{
         const subjectsCollection = await Subject.findAll();
         res.status(200).send(subjectsCollection);
     } catch (e) {
-        console.log(e)
-      res.status(500).send(e)
+        console.log(e);
+      res.status(500).send(e);
+    }
+}
+exports.getSubjectsBySemesterAndBranch = async(req,res)=>{
+    try {
+        const subjectsCollection = await Subject.findAll({
+            where:{
+                semester:req.body.semester,
+                branch:req.body.branch
+            }
+        });
+        res.status(200).send(subjectsCollection);
+    } catch (e) {
+        console.log(e);
+      res.status(500).send(e);
     }
 }
